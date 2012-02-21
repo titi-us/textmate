1e1475155dc06e917a2a6d7615184f40ea742d7d
Joachim Mårtensson <joachimmartensson@gmail.com>
2012-02-21 23:36:02 +0100

    Make rules know their own id, make sure rules don't get duplicated. Introduce rational based number, switch to using vector based backing instead of maps

--- Frameworks/theme/tests/t_theme.cc
+++ Frameworks/theme/tests/t_theme.cc
@@ -3,7 +3,27 @@
 
 class ThemeTests : public CxxTest::TestSuite
 {
-	int repeat = 10000;
+	class npow2 {
+		enum {one = 1<<32};
+		int backing;
+		npow2():backing(0) {}
+
+		npow2(int exponent) {
+			backing = one>>exponent;
+		}
+		bool operator >(npow2 rhs) {
+			return backing > rhs.backing;
+		}
+		npow2& operator -= (npow2 rhs) {
+			backing =-rhs.backing;
+			return *this;
+		}
+		npow2& operator += (npow2 rhs) {
+			backing +=rhs.backing;
+			return *this;
+		}
+	};
+	int repeat = 1;
 	public:
 	struct pathx_t;
 	struct pathx_t {
@@ -11,13 +31,15 @@ class ThemeTests : public CxxTest::TestSuite
 		bool match;
 		int possible;
 		int route;
+		int id;
 		pathx_t& operator[](std::string str) {
 			return _path[str];
 		}
-		pathx_t& set(bool b,int m, int r) {
+		pathx_t& set(bool b,int m, int r, int i) {
 			match = b;
 			possible = m;
 			route = r;
+			id = i;
 			return *this;
 		}
 
@@ -81,20 +103,22 @@ class ThemeTests : public CxxTest::TestSuite
 		}
 		
 		iterate(it, ordering) {
-			// printf("item: %.4f %s\n", it->first, it->second.c_str());
+			printf("item: %.4f %s\n", it->first, it->second.c_str());
 			total += it->first;
 		}
 	   }
-		printf("%.4f seconds to computed oldstyle:%.1f\n", timer1.duration(), total);
+	 	printf("%.4f seconds to computed oldstyle:%.1f\n", timer1.duration(), total);
 		
 	}
 	struct rules_t {
+		rules_t(size_t sz) : matchingSelectors(sz){}
 		struct internal_t {
 			struct pattern_t{
 				int pattern;
 				int mask;
 				int size;
-			};
+			}; 
+			int id;
 			std::vector<pattern_t>  matchingSelectors;
 			pattern_t operator[](size_t idx) { return matchingSelectors[idx];}
 			internal_t& add(int p, int m, int s) {
@@ -112,7 +136,7 @@ class ThemeTests : public CxxTest::TestSuite
 				return matchingSelectors.size();
 			}
 		} internal;
-		std::map<size_t, internal_t> matchingSelectors;
+		std::vector<internal_t> matchingSelectors;
 		
 		internal_t& operator[](size_t idx) {
 			return matchingSelectors[idx];
@@ -120,22 +144,22 @@ class ThemeTests : public CxxTest::TestSuite
 	};
 	void test_scope_theme_benchmark ()
 	{
-		rules_t matchingSelectors;
+		rules_t matchingSelectors(8);
 		// path - mask
 		//"text.* markup.bold", 
-		matchingSelectors[0].add(3<<4|1<<1,7<<4|1<<1).add( 5<<4|3<<1, 7<<4|7<<1);
+		matchingSelectors[0].add(3<<4|1<<1,7<<4|1<<1).add( 5<<4|3<<1, 7<<4|7<<1).id = 0;
 		//"text markup.bold"
-		matchingSelectors[1].add(3<<4, 7<<4).add( 5<<4|3<<1, 7<<4|7<<1);
+		matchingSelectors[1].add(3<<4, 7<<4).add( 5<<4|3<<1, 7<<4|7<<1).id = 1;
 		//"text.html meta.*.markdown markup"
-		matchingSelectors[3].add(3<<4|5<<1, 7<<4|7<<1).add( 7<<4|1<<1|1<<0, 7<<4|1<<1|1<<0).add( 5<<4, 7<<4);
+		matchingSelectors[3].add(3<<4|5<<1, 7<<4|7<<1).add( 7<<4|1<<1|1<<0, 7<<4|1<<1|1<<0).add( 5<<4, 7<<4).id= 3;
 		//"text.html meta.* markup"
-		matchingSelectors[4].add(3<<4|5<<1, 7<<4|7<<1).add( 7<<4|1<<1, 7<<4|1<<1).add( 5<<4, 7<<4);
+		matchingSelectors[4].add(3<<4|5<<1, 7<<4|7<<1).add( 7<<4|1<<1, 7<<4|1<<1).add( 5<<4, 7<<4).id=4;
 		//"text.html * markup" 
-		matchingSelectors[5].add(3<<4|5<<1, 7<<4|7<<1).add( 1<<4, 1<<4).add( 5<<4,7<<4);
+		matchingSelectors[5].add(3<<4|5<<1, 7<<4|7<<1).add( 1<<4, 1<<4).add( 5<<4,7<<4).id=5;
 		 //"text.html markup"
-		matchingSelectors[6].add(3<<4|5<<1, 7<<4|7<<1).add( 5<<4, 7<<4);
+		matchingSelectors[6].add(3<<4|5<<1, 7<<4|7<<1).add( 5<<4, 7<<4).id=6;
 		//"text markup"
-		matchingSelectors[7].add(3<<4, 7<<4).add( 5<<4, 7<<4); 
+		matchingSelectors[7].add(3<<4, 7<<4).add( 5<<4, 7<<4).id=7; 
 		// [text 3<<4,markup 5<<4,meta 7<<4,* 1<<4][bold 3<<1,html 5<<1,* 1<<1][markdown 1]
 		
 		static std::string const selectorStrings[] =
@@ -154,14 +178,14 @@ class ThemeTests : public CxxTest::TestSuite
 		};
 		
 		pathx_t root;
-		root["text"].set(true,1<<1|1<<7, 3<<4)["html"]
-			.set(true,1<<0|1<<3|1<<4|1<<5|1<<6|  1<<1|1<<7, 3<<4|5<<1);//1<<0*
-		root["text"]["*"].set(false,1<<0  |1<<1|1<<7, 3<<4|1<<1);
+		root["text"].set(true,1<<1|1<<7, 3<<4, 10)["html"]
+			.set(true,1<<0|1<<3|1<<4|1<<5|1<<6|  1<<1|1<<7, 3<<4|5<<1,9);//1<<0*
+		root["text"]["*"].set(false,1<<0  |1<<1|1<<7, 3<<4|1<<1,-1);
 		int help = 1 << 3 | 1 << 4 | 1 << 5 | 1 << 6| 1<<7;
-		root["markup"].set(true, help, 5<<4)["bold"]
-			.set(true,1<<0|1<<1|help, 5<<4|3<<1);
-		root["*"].set(false,1<<5,1<<4);
-		root["meta"]["*"].set(false,1<<4,7<<4|1<<1)["markdown"].set(false,1<<3  |1<<4, 7<<4|1<<1|1<<0);
+		root["markup"].set(true, help, 5<<4, 8)["bold"]
+			.set(true,1<<0|1<<1|help, 5<<4|3<<1,2);
+		root["*"].set(false,1<<5,1<<4,-1);
+		root["meta"]["*"].set(false,1<<4,7<<4|1<<1,-1)["markdown"].set(false,1<<3  |1<<4, 7<<4|1<<1|1<<0,-1);
 		
 		auto p = std::vector<std::vector<std::string> >();
 		{
@@ -194,6 +218,7 @@ class ThemeTests : public CxxTest::TestSuite
 			total = 0.0;
 			
 			size_t s = p.size();
+			std::vector<double> collector(11); 
 			std::multimap<double, std::string> ordering;
 			rules_t::internal_t v;
 			int power = 0;
@@ -204,7 +229,7 @@ class ThemeTests : public CxxTest::TestSuite
 			int j = 0;
 			int sz = p[s].size();
 			power += sz;
-						
+
 			while(pathx_t* current = next(p[s][j], *path))
 			{
 				j++;
@@ -214,13 +239,13 @@ class ThemeTests : public CxxTest::TestSuite
 					double score = 0;
 					for(size_t k = 0; k < j; ++k)
 						score += 1 / pow(2, power - k);
-					ordering.insert(std::make_pair(score, p[s][j-1]));
+					collector[path->id] = score;
 				}
 			}
 			computed = computed | path->possible;
 			v.add(path->route, 0, sz);
 			
-		}
+		} 
 		/*
 		iterate(it, v) {
 			// printf("vector item: %#x \n", *it);
@@ -232,15 +257,24 @@ class ThemeTests : public CxxTest::TestSuite
 			
 			double rank;
 			if(does_match(v, matchingSelectors[idx - 1],&rank) ) {
-				ordering.insert(std::make_pair(rank, selectorStrings[idx-1]));
+				double& val = collector[idx-1];
+				if( val < rank)
+					val = rank;
+					
 				// printf("---ffs %d\n matched",idx);
 
 			}
 			computed &= ~(1<<idx-1);
 		}
+		int index = 0;
+		iterate(it, collector) {
+			printf("collector item: %.4f %d\n", *it, index);
+
+			ordering.insert(std::make_pair(*it, selectorStrings[index++]));
+		}
 		
 		iterate(it, ordering) {
-			// printf("item: %.4f %s\n", it->first, it->second.c_str());
+			printf("item: %.4f %s\n", it->first, it->second.c_str());
 			total += it->first;
 		}
 	   		
